254a878dec8b186b335f7c84b72faaffa2f6eb05
RobertArustamyan <robertarustamyan2@gmail.com>
2024-07-23 10:12:12 +0400

    Created API endpoint

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from blueprints.configurator.configurator import conf_bp
 
 app = Flask(__name__)
-app.register_blueprint(conf_bp)
+CORS(app)
+app.register_blueprint(conf_bp, url_prefix='/conf')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- BackEnd/blueprints/configurator/configurator.py
+++ BackEnd/blueprints/configurator/configurator.py
@@ -1,8 +1,52 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from BackEnd.Functions.ImageModification.ColorChanging import ImageMod
 
 conf_bp = Blueprint('conf', __name__)
 
 
-@conf_bp.route('/')
+@conf_bp.route('/test')
 def index():
     return 'Test'
+
+
+@conf_bp.route('/process', methods=['POST'])
+def process():
+    try:
+        data = request.get_json()
+        if not data or 'image_base64' not in data or 'color_conf' not in data:
+            return jsonify({'error': 'Invalid data format'}), 400
+
+        processed_data = data_processing(data)
+        return jsonify({'processed_image': processed_data}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+def data_processing(data):
+    image_converter = ImageMod(data['image_base64'])
+    image_converter.change_colours(data['color_conf'])
+    return image_converter.image_data_base64
+
+
+"""
+DATA format example
+{
+    'image_base64' : 'ImageBase64' (string),
+    'color_conf': [
+            'ColorRange': {
+                'FromColor': (0, 0, 0),
+                'ToColor': (20, 20, 20)
+                },
+            'ReplaceColor': (255, 255, 255)
+            },
+            {
+            'ColorRange': {
+                'FromColor': (255, 255, 255),
+                'ToColor': (200, 200, 200)
+                },
+            'ReplaceColor': (0, 0, 0)
+        }
+    ]
+}
+"""
