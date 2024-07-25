0c06eafc65b2351e055fa57e901985c2925c0f0f
RobertArustamyan <robertarustamyan2@gmail.com>
2024-07-25 23:58:51 +0400

    Added invert-image and grayscale-image endpoints

--- BackEnd/blueprints/configurator/configurator.py
+++ BackEnd/blueprints/configurator/configurator.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from BackEnd.Functions.ImageModification.ColorChanging import ImageMod
-
+from BackEnd.Functions.ImageModification.DataProcessing import DataProcessor
 conf_bp = Blueprint('conf', __name__)
 
 @conf_bp.route('/process-image', methods=['POST'])
@@ -9,38 +9,33 @@ def process():
         data = request.get_json()
         if not data or 'image_base64' not in data or 'color_conf' not in data:
             return jsonify({'error': 'Invalid data format'}), 400
-
-        processed_data = image_data_processing(data)
-        return jsonify({'processed_image': processed_data}), 200
+        processor = DataProcessor(image_body=data, convert_format='configuration')
+        return jsonify({'processed_image': processor.data_processing}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@conf_bp.route('/grayscale-image',methods=['POST'])
+def grayscale():
+    try:
+        data = request.get_json()
+        if not data or 'image_base64' not in data:
+           return jsonify({'error': 'Invalid data format'}), 400
+        processor = DataProcessor(image_body=data, convert_format='grayscale')
+        return jsonify({'processed_image': processor.data_processing}), 200
 
-def image_data_processing(image_body):
-    image_converter = ImageMod(image_body['image_base64'])
-    image_converter.change_colors(image_body['color_conf'])
-    return image_converter.image_data_base64
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
 
 
-"""
-DATA format example
-{
-    'image_base64' : 'ImageBase64' (string),
-    'color_conf': [
-            'ColorRange': {
-                'FromColor': (0, 0, 0),
-                'ToColor': (20, 20, 20)
-                },
-            'ReplaceColor': (255, 255, 255)
-            },
-            {
-            'ColorRange': {
-                'FromColor': (255, 255, 255),
-                'ToColor': (200, 200, 200)
-                },
-            'ReplaceColor': (0, 0, 0)
-        }
-    ]
-}
-"""
+@conf_bp.route('/invert-image',methods=['POST'])
+def invert():
+    try:
+        data = request.get_json()
+        if not data or 'image_base64' not in data:
+            return jsonify({'error': 'Invalid data format'}), 400
+        processor = DataProcessor(image_body=data, convert_format='invert')
+        return jsonify({'processed_image': processor.data_processing}), 200
+
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
